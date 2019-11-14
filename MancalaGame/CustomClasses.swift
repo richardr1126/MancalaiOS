5a1ccde79dd57249cf7a0998415390f1c97dcaea
Richard Roberson <richardr1126@gmail.com>
2019-11-14 00:26:21 -0600

    stack overflow version

---
+++ MancalaGame/CustomClasses.swift
@@ -0,0 +1,24 @@
+//
+//  MancalaGame
+//
+//  Created by Richard Roberson on 11/13/19.
+//  Copyright © 2019 Richard Roberson. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class Chip: SKSpriteNode {
+    
+
+}
+
+class Pit: SKNode {
+    var chipsNode = SKNode()
+    var chips: [Chip] = []
+    
+    
+    
+    
+    
+}
